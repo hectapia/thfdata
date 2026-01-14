d5d3a3896a079f110ceb5e7689795c330c4f7ae3
hectapia <oli21007@byui.edu>
2026-01-13 23:07:45 -0600

    update configuration

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// This file is ignored by Git. 
+// It loads credentials into a global object for the app to use.
+window.SUPABASE_CONFIG = {
+    URL: 'https://outdecjeezousqqvaidt.supabase.co',
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dGRlY2plZXpvdXNxcXZhaWR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyNjA4MTUsImV4cCI6MjA4MzgzNjgxNX0.NYsYZsXuAw11dhVN71n7xPpSUAq0G6c_eDgC0C5-yRA'
+};
